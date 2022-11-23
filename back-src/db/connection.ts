0c2d4e04f1822ee20f682abad4041dc41f97a93a
Javier Torales <82301704+JtoralesK@users.noreply.github.com>
2022-11-23 07:23:33 -0300

    se saco puerto

--- back-src/db/connection.ts
+++ back-src/db/connection.ts
@@ -5,7 +5,6 @@ export const sequelize = new Sequelize(path,{
   username: process.env.USERNAME_SEQUALIZE,
   password: process.env.PASSWORD_SEQUELIZE,
   database: "d4liiqmqa0iq1n",
-  port: 5432,
   host: "ec2-52-70-205-234.compute-1.amazonaws.com",
   ssl: true,
   // esto es necesario para que corra correctamente
